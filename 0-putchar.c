e0674ec1e01cc4fb72c9df74c07c301dc2cbbd8e
Ojo olalekan <olalekanoj89@gmail.com>
2022-07-05 03:28:16 -0700

    Test Y

---
+++ 0-putchar.c
@@ -0,0 +1,29 @@
+#include "davis.h"
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char *sh = "Davis";
+	while (*sh)
+
+	{
+
+		_putchar(*sh);									sh++;
+											}
+
+	_putchar('\n');
+
+	return (0);
+
+}
